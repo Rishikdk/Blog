66457efb7c88d5b97ba062c808dda5184c2c60ff
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-09-01 16:23:05 +0545

    setup route

--- Rental/src/App.jsx
+++ Rental/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState, useEffect } from 'react'
-import { getPosts, getPost, createPost,updatePost,deletePost} from './api'
 import './App.css'
-
-
+import {HashRouter as Router, Routes, Route} from "react-router-dom"
+import {About} from '../pages/About'
+import { Contact } from '../pages/Contact'
+import {CreateBlog} from '../pages/CreateBlog'
+import { Home } from '../pages/Home'
+import { Landing } from '../pages/Landing'
+import { Profile } from '../pages/Profile'
+import { ReadBlog } from '../pages/ReadBlog'
 
 
 function App() {
@@ -16,9 +20,17 @@ function App() {
 
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path='/landing' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/createblog' element={<CreateBlog/>}/>
+        <Route path='/readblog' element={<ReadBlog/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </Router>
   )
 }
 
