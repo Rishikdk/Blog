b51d2d3ddd9da305b7b71cc423f9a83262da7955
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-09-01 10:48:26 +0545

    connect successful

--- backend/connect.js
+++ backend/connect.js
@@ -1,7 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config({path:"./config.env"})
-// const uri = "mongodb+srv://rishikdk71:<db_password>@cluster0.ifios.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.ATLAS_URI, {
