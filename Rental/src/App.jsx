f20e3ba27250442c300a09300eb43656ebd6abca
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-09-01 14:31:15 +0545

    data send to db

--- Rental/src/App.jsx
+++ Rental/src/App.jsx
@@ -5,23 +5,36 @@ import axios from "axios" //used to geab data to fetch data
 
 function App() {
   const [data, setData]= useState()
-  
+  //data send to the data base
+  function createPost(){
+    let postObject ={
+      title:"The data",
+      description:"The love of the world",
+      contain:"about my self",
+      dataCreated: new Date()
+    }
+    axios.post("http://localhost:3000/posts", postObject)
+  }
 
   //function to get data from backend
-  useEffect(()=>{
-      async function grabData() {
-        const response = await axios.get("http://localhost:3000/posts")
-        if(response.status === 200){
-          setData(response.data)
-        }
-      }
-      grabData()
-  },[])
+  // useEffect(()=>{
+  //     async function grabData() {
+  //       const response = await axios.get("http://localhost:3000/posts")
+  //       if(response.status === 200){
+  //         setData(response.data)
+  //       }
+  //     }
+  //     grabData()
+  // },[])
 
 
   return (
     <>
-    {JSON.stringify(data)}
+    {/* {JSON.stringify(data)} */}
+    <button onClick={createPost}>
+      Create Object
+
+    </button>
     </>
   )
 }
