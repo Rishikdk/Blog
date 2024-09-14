3a32109c7abeb0fc9d009d5ce043f065ee0ae27e
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-09-14 21:48:53 +0545

    updates

--- Rental/src/api.js
+++ Rental/src/api.js
@@ -72,7 +72,6 @@ export async function updateUser(id,user){
 }
 
 export async function verifyUser(user) {
-
     const response = await axios.post(`http://localhost:3000/users/login`,user)
     if(response.data.success){
         return response.data.token
