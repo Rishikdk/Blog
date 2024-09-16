1bbaa0c0b29ecbac72a3cca8f313a03f04e506db
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-09-16 18:34:45 +0545

    updates

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT  || 3000
 
 app.use(cors(
     {
-        origin: ["https://blog-two-xi-54.vercel.app/"],
+        origin: [""],
         methods:["POST","GET"],
         credentials:true
     }
